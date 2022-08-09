17be72f055d16788099f465af1a81550d663228a
15ariko <110599821+15ariko@users.noreply.github.com>
2022-08-09 10:24:19 +0300

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -4,6 +4,8 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+let a=parseInt(result.input);
+let s=parseInt(6*a**2)
+let v=(a**3)
+console.log(s,v);  
 });
