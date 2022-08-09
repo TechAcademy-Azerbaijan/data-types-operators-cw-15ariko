fdb917092370b567de6c59124ab473ec0aba0bc7
15ariko <110599821+15ariko@users.noreply.github.com>
2022-08-09 10:08:58 +0300

    Update main.js

--- task-8/main.js
+++ task-8/main.js
@@ -4,6 +4,9 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+let a=parseInt(result.input)
+let b=parseInt(a/10)
+let c=(a%10)
+let netice = c*100 + b*1
+console.log(netice)  
 });
