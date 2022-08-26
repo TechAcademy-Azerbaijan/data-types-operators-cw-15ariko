aa39705dd461484c4d930d21b66d60b89490229f
15ariko <110599821+15ariko@users.noreply.github.com>
2022-08-26 11:39:31 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -4,6 +4,10 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+let input = result.input;
+  let number = input.split(",");
+  let x = parseInt(number[0]);
+  let n = parseInt(number[1]);
+  let a = x ** n;
+  console.log(a);  
 });
